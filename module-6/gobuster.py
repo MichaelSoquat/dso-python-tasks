8da7fea35d55bde434449513cacb419a91dd2932
MichaelSoquat <michael.soquat@gmx.de>
2024-08-19 16:02:26 +0200

    added gobuster

---
+++ module-6/gobuster.py
@@ -0,0 +1,67 @@
+import argparse
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+import time
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Own 'Gobuster' with selenium")
+    parser.add_argument("-url", "--url", type=str, required=True, help="Enter the base URL")
+    parser.add_argument("-wordlist", type=str, required=True, help="Path of wordlist")
+    args = parser.parse_args()
+
+    url = args.url
+    wordlist_path = args.wordlist
+
+    # Configuration of selenium
+    driver_path = "/usr/local/bin/geckodriver" 
+    options = Options()
+    options.add_argument("--headless")
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Firefox(service=service, options=options)
+
+    # Get the base content only once
+    try:
+        driver.get(url)
+        time.sleep(2)  # Ensure the page is fully loaded
+        base_content = driver.page_source
+    except Exception as e:
+        print(f"An error occurred when fetching the base URL: {str(e)}")
+        driver.quit()
+        return
+
+    # Check dirs
+    def check_for_dirs(driver, base_url, directory):
+        full_url = f"{base_url}/{directory}"
+        
+        try:
+            driver.get(full_url)
+            time.sleep(2)  # Ensure the page is fully loaded
+            
+            full_content = driver.page_source
+            
+            if base_content != full_content:
+                print(f"[+] Found: {full_url}")
+            else:
+                print(f"[-] Not found: {full_url}")
+        
+        except Exception as e:
+            print(f"An error occurred with {full_url}: {str(e)}")
+            
+    # Open wordlist and check lines
+    try:
+        with open(wordlist_path, 'r') as file:
+            dirs = [line.strip() for line in file]
+        for dir in dirs:
+            check_for_dirs(driver, url, dir)
+    except FileNotFoundError:
+        print(f"Wordlist not found under {wordlist_path}.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
